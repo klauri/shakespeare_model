f5f2a7f55378d123ed84e3ff0cd4ce5e227e6fd0
Kirk Lauri <kirk.lauri@live.com>
2023-06-23 23:30:13 -0400

    added dropout

--- train.py
+++ train.py
@@ -7,12 +7,12 @@ batch_size = 64 # how many independent sequences will we process in parallel
 block_size = 256 # what is the maximum context length for predictions
 max_iters = 5000
 eval_interval = 500
-learning_rate = 3e-4
+learning_rate = 1e-4
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 eval_iters = 200
-n_embd = 384 
-n_head = 6
-n_layer = 6
+n_embd = 32 
+n_head = 2
+n_layer = 2
 dropout = 0.2
 # --------------------
 
@@ -187,6 +187,7 @@ m = model.to(device)
 
 # create a PyTorch optimizer
 optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate)
+print(sum(p.numel() for p in m.parameters())/1e6, 'M parameters')
 
 for iter in range(max_iters):
 
